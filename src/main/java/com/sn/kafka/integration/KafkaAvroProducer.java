f7b24469607e9243e73846eb42bb2208fa81adf9
denniskuipersSN <61538995+denniskuipersSN@users.noreply.github.com>
2020-02-27 15:28:39 +0100

    Maven Support

---
+++ src/main/java/com/sn/kafka/integration/KafkaAvroProducer.java
@@ -0,0 +1,110 @@
+package com.sn.kafka.integration;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class KafkaAvroProducer {
+
+    public KafkaAvroProducer(){
+
+    }
+
+    private static void ProducerClient(Properties producerProp)
+    {
+
+        String topic      = producerProp.getProperty("topic");
+        String SchemaRegistryURL = producerProp.getProperty("schema_url");
+        String ProducerConfigFile = producerProp.getProperty("ProducerConfgFile");
+
+        Properties props = new Properties();
+        Properties propsProducer = new Properties();
+        try (InputStream input = new FileInputStream(ProducerConfigFile)) {
+            propsProducer.load(input);
+            props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, propsProducer.getProperty("bootstrap.servers"));
+            if (propsProducer.containsKey("ssl.truststore.location")) {
+                props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, propsProducer.getProperty("ssl.truststore.location"));
+                props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, propsProducer.getProperty("ssl.truststore.password"));
+                props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, propsProducer.getProperty("ssl.keystore.location"));
+                props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, propsProducer.getProperty("ssl.key.password"));
+            }
+        }catch (Exception e){
+
+        }
+
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, topic);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, SchemaRegistryURL);
+        KafkaProducer producer = new KafkaProducer(props);
+
+        String key = "key1";
+        String userSchema = "{\"type\":\"record\"," +
+                "\"name\":\"myrecord\"," +
+                "\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}";
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse(userSchema);
+        GenericRecord avroRecord = new GenericData.Record(schema);
+        avroRecord.put("f1", "value1");
+        System.out.println("Sending Message" + avroRecord.getSchema().toString());
+        ProducerRecord<String, Object> record = new ProducerRecord<>("testAvro12", key, avroRecord);
+        try {
+            producer.send(record);
+            System.out.println(producer.metrics());
+        } catch (Exception e) {
+             e.printStackTrace();
+            // may need to do something wth it
+        }
+        // When you're finished producing records, you can flush the producer to ensure it has all been written to Kafka and
+        // then close the producer to free its resources.
+        try {
+            System.out.println("Flush");
+            producer.flush();
+            System.out.println("Finished flush");
+            producer.close();
+            System.out.println("Finished close");
+        }catch (Exception e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("Finished");
+    }
+
+    private static Properties getConfigFile(String[] args){
+        CliArgs cliArgs = new CliArgs(args);
+        String configfile   = cliArgs.switchValue("-configfile");
+        System.out.println("ConfigFile : " + configfile);
+        Properties prop = new Properties();
+        try (InputStream input = new FileInputStream(configfile)) {
+            prop.load(input);
+            String log4jConfigFile = prop.getProperty("log4jfile");
+            System.out.println("log4j file : " + log4jConfigFile);
+            PropertyConfigurator.configure(log4jConfigFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.setSecurityManager(null);
+        Properties prop = getConfigFile(args);
+        KafkaAvroProducer kafkaAvroProducer = new KafkaAvroProducer();
+        kafkaAvroProducer.ProducerClient(prop);
+        System.out.println("Done");
+    }
+}
---
+++ src/main/java/com/sn/kafka/integration/KafkaClientConsumerTestSN.java
@@ -0,0 +1,123 @@
+package com.sn.kafka.integration;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+
+public class KafkaClientConsumerTestSN {
+
+    public KafkaClientConsumerTestSN() {
+
+    }
+
+    private static Consumer<String, GenericRecord> createConsumer(Properties props) {
+
+        String topic = props.getProperty("topic");
+        String SchemaRegistryURL = props.getProperty("schema_url");
+        String ConsumerConfigFile = props.getProperty("ConsumerConfgFile");
+
+        Properties propsConsumer = new Properties();
+        try (InputStream input = new FileInputStream(ConsumerConfigFile)) {
+
+            propsConsumer.load(input);
+            if (props.containsKey("ssl.truststore.location")) {
+                propsConsumer.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, props.getProperty("ssl.truststore.location"));
+                propsConsumer.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, props.getProperty("ssl.truststore.password"));
+                propsConsumer.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, props.getProperty("ssl.keystore.location"));
+                propsConsumer.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, props.getProperty("ssl.key.password"));
+            }
+            propsConsumer.put("bootstrap.servers",propsConsumer.get("bootstrap.servers"));
+            propsConsumer.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            propsConsumer.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+            propsConsumer.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+            propsConsumer.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, SchemaRegistryURL);
+            propsConsumer.put(ConsumerConfig.GROUP_ID_CONFIG, propsConsumer.getProperty("group.id"));
+
+            for (Object key: propsConsumer.keySet()) {
+                System.out.println(key + ": " + propsConsumer.getProperty(key.toString()));
+            }
+
+        }catch (IOException io) {
+            io.printStackTrace();
+        }
+        //String groupid = props.getProperty("group1");
+        // Create the consumer using props.
+        final Consumer<String, GenericRecord> consumer = new KafkaConsumer<String, GenericRecord>(propsConsumer);
+        //Consumer<String,String> consumer = new KafkaConsumer<String, String>(props);
+
+        // Subscribe to the topic.
+        consumer.subscribe(Arrays.asList(topic));
+        //consumer.subscribe(Collections.singletonList(topic));
+        //System.out.println(consumer.listTopics());
+        System.out.println("Connected and connected to topic : " + topic);
+        return consumer;
+    }
+
+    private static String runConsumer(Properties prop) throws InterruptedException {
+        StringBuilder stringBuilder = new StringBuilder(100);
+        String s = "";
+        try (Consumer<String, GenericRecord> consumer = createConsumer(prop)) {
+            Duration duration = Duration.ofSeconds(10);
+            try {
+                    ConsumerRecords<String, GenericRecord> records = consumer.poll(duration);
+                    for (ConsumerRecord<String, GenericRecord> record : records) {
+                        System.out.printf("offset = %d, key = %s, value = %s \n", record.offset(), record.key(), record.value());
+                        stringBuilder.append(","+record.value());
+                    }
+                    System.out.println("Reading done :" + records.count());
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            finally {
+                consumer.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static Properties getConfigFile(String[] args){
+        CliArgs cliArgs = new CliArgs(args);
+        String configfile   = cliArgs.switchValue("-configfile");
+        System.out.println("ConfigFile : " + configfile);
+        Properties prop = new Properties();
+        try (InputStream input = new FileInputStream(configfile)) {
+            prop.load(input);
+            String log4jConfigFile = prop.getProperty("log4jfile");
+            System.out.println("log4j file : " + log4jConfigFile);
+            PropertyConfigurator.configure(log4jConfigFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public static String runKafkaClient(String[] args) throws Exception {
+
+        Properties prop = getConfigFile(args);
+        System.setSecurityManager(null);
+        System.out.println("Start Consumer");
+        return runConsumer(prop);
+    }
+    public static void main(String[] args) throws Exception {
+        Properties prop = getConfigFile(args);
+        String Messages = runConsumer(prop);
+        System.out.println(Messages);
+    }
+
+}
