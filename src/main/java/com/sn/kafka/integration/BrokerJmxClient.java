e9ee52b4adb8d892c573782158b8b736f4fe4cb9
denniskuipersSN <61538995+denniskuipersSN@users.noreply.github.com>
2020-03-30 10:39:47 +0200

    WI0001007
    Signed-off-by: denniskuipersSN <61538995+denniskuipersSN@users.noreply.github.com>

--- src/main/java/com/sn/kafka/integration/BrokerJmxClient.java
+++ src/main/java/com/sn/kafka/integration/BrokerJmxClient.java
@@ -94,7 +94,7 @@ public class BrokerJmxClient
                   System.out.println ("Send ; " + MeterMetric);
               }
             }
-              Thread.sleep (30000000);
+              Thread.sleep (300000000);
            }catch (Exception e)
           {
               System.out.println (e);
